59e0541570697956cdc32637c43db020d4a7d829
nachokhan <ignacio.rigoni@gmail.com>
2021-05-23 22:37:58 -0300

    hermoso y casi terminado

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask_socketio import SocketIO, emit
-from flask import Flask, render_template, url_for, request
+from flask import Flask, render_template, request
 from time import sleep
 from pubsub import pub
 
@@ -11,28 +11,33 @@ app.config['DEBUG'] = True
 # turn the flask app into a socketio app
 socketio = SocketIO(app, async_mode=None, logger=False, engineio_logger=True)
 
+
 def listener(arg1):
     print('Function listener1 received:')
     print('  arg1 =', arg1)
-    socketio.emit('newmessage',{'message':arg1})
+    socketio.emit('newmessage', {'message': arg1})
 
 
 pub.subscribe(listener, 'rootTopic')
+
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
     return render_template('index.html')
 
-@app.route('/post', methods=['POST'])
+
+@app.route('/post', methods=['GET', 'POST'])
 def post():
-    pub.sendMessage('rootTopic', arg1='post')
-    return "posteeeed"
+    texto = request.args.get('texto')
+    if texto:
+        pub.sendMessage('rootTopic', arg1=texto)
+    return render_template("post.html")
+
 
 @socketio.on('connect')
 def connect():
     pub.sendMessage('rootTopic', arg1='connected to socket')
     print('Client connected')
-
 
 
 if __name__ == '__main__':
